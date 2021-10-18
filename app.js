0e4ec19d7f0b68323755572a559cc1b9e22a505b
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-18 19:07:40 +0400

    primitive data-types in Js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-console.log("Hello World !")
-console.warn("Hello World !")
-console.error("Hello World !")
+//string
+//typeof used to see the datatype
+console.log(typeof("Hello World !"))
+console.log(typeof("d"))
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 12343268097683621)
+console.log(typeof -12343268097683621)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
