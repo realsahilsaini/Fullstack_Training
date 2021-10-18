84c2ec6850133cceccf98eed916cdf51018747fd
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-18 20:35:14 +0400

    operators

--- app.js
+++ app.js
@@ -1,17 +1,80 @@
-//Reassigning the value to variable
+//Operators
 
-// var age =19
+//**Arithmetic**
+// var a = 2
+// var b = 10
 
-// age = 21
+// console.log("a+b = ", a+b)
+// console.log("a-b = ", a-b)
+// console.log("a*b = ", a*b)
+// console.log("a/b = ", a/b)
+// console.log("b^a = ", b**a)
+// console.log("b%a = ", b%a)
 
-// console.log(age)
+//**Assignment Operators**
+// x = 10
+// console.log(x)
 
-// --------------------------------------------------------
+// Here we are incrementing the value of x 
+// x+=1
+// console.log(x)
 
-//To make a variable vbalue as a constant 
-const age = 19
+// Here we are decrementing the value of x 
+// x-=1
+// console.log(x)
 
-// This will give an error, beacause we have made the age variable as a constant
-age =21
+// Here we are post-incrementing the value of x 
+// x+=1
+// console.log(x)
 
-console.log(21)
+// Here we are post-decrementing the value of x 
+// x--
+// console.log(x)
+
+
+
+//**Comparison Operator**
+
+var d = 10
+var e = 2
+var f = '10'
+// Outputs will be bool values 
+// console.log(d > e)
+// console.log(d < e)
+// console.log(d >= e)
+// console.log(d <= e)
+// console.log(d == e)
+
+//Here third equal to sign checks for the datatype
+// console.log(d === e)
+
+// Here the number d is type casted to string & Output: 1010
+// console.log(d + f)
+
+//**Logical operator** 
+
+// Logical AND 
+//In Js only true && true evaluates to true
+
+// console.log('true && true:',true && true)
+// console.log('true && false:',true && false)
+// console.log('false && true:',false && true)
+// console.log('false && false:',false && false)
+
+// Logical OR 
+//In Js only false or false evaluates to false
+
+console.log('true || true:',true || true)
+console.log('true || false:',true || false)
+console.log('false || true:',false || true)
+console.log('false || false:',false || false)
+
+
+
+
+
+
+
+
+
+
