edd8f173bf1650c699c8ebd2538af1efc17001a5
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-19 09:27:38 +0400

    DOM API Selection Methods

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 var heading = document.getElementById('heading')
+// heading.innerHTML= "Hi from Js"
 
-console.log(typeof heading)
+// var para = document.getElementsByClassName('para')
+// console.log(heading)
+
+// console.log(para.length)
+
+// Output is the firs element with the 'para' as a class name
+// console.log(para[0])
+
+// Output is the second element with the 'para' as a class name
+// console.log(para[1])
+
+
+// var tags = document.getElementsByTagName('a')
+// console.log(tags)
+
+// var query = document.querySelector('#heading')
+// console.log(query)
+
+var query = document.querySelector('.para')
+console.log(query)
