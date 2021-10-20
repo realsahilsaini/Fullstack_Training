3982034e02a984966dd95dd19a0d8448173fd3e8
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-20 14:58:38 +0400

    Array methods

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
-// const a = document.querySelectorAll('input')
-// const b = document.querySelectorAll('input')
-// OR 
-const a = document.getElementsByTagName('input')[0]
-const b = document.getElementsByTagName('input')[1]
+// Arrays and its methods
+var arr = [1,2,3,4,5, 6, 7, 8]
 
-const add = document.querySelector('#add')
-const multiply = document.querySelector('#multiply')
-const resultbox = document.querySelector('.result')
+// arr.push(6);
+// pushes new element to last of the array 
 
 
-const calculate = (event, operation) => {
-    // if(operation == 'add'){
-    //     console.log('adding')
-    // }
-    // else if(operation =='multiply'){
-    //     console.log('multiply')
-    // }
-    switch(operation){
-        case 'add':
-        resultbox.innerHTML =  parseInt(a.value) + parseInt(b.value)
-        break;
-
-        case 'multiply':
-        resultbox.innerHTML =  parseInt(a.value) * parseInt(b.value)
-        break;
-
-        default:
-            resultbox.innerHTML = "Not a valid input"
-    }
-}
+// arr.unshift(6)
+// pushes new element to beginning of the array 
 
 
-// Here we are calling function inside another function 
-add.addEventListener('click',(event) =>{
-    return calculate(event, 'add')
-})
+// arr.shift()
+// Delete the first element of the array
 
-multiply.addEventListener('click', (event) =>{
-    return calculate(event, 'multiply')
-})
+
+//To select particular part of the array
+//we'll use slice
+/*remember: 
+1. Here slice will make a new array not like others where they directly made changes to the array itself.
+2. Here the first index is inclusive and last is exclusive
+*/
+// var new_arr = (arr.slice(2,5)) 
+// console.log(new_arr)
+
+
+//To print even number from the array
+//First approach
+// var new_arr = [] 
+// for(i=0; i< arr.length; i++){
+//     if(arr[i]%2==0) new_arr.push(arr[i])
+// }
+
+// console.log(new_arr)
+
+//Optimized approach using built-in methods
+// var new_arr = arr.filter((element, index)=>{
+//     if(element%2==0) return true
+//     // Here whenever it return true element will be added in the new array 
+//     // console.log(element, index)
+// })
+
+
+//To remove specific element in the array using built-in 
+//Parameters: (index, no. of elements to remove after the index)
+// arr.splice(3, 3)
+
+// This will output all the removed elements 
+console.log(arr.splice(3, 3))
+
+
+console.log(arr)
+
+
+
