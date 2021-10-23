c65273e1ef0069f480133cde4c82eb67fbdf8367
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-23 21:45:02 +0400

    Obejcts & Methods

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ let car = {
     // A function inside a object is a method 
     print: function() {
         console.log('Car')
-        console.log( this.name + ' by ' + this.manufacturer)
+        // console.log( this.name + ' by ' + this.manufacturer)
+        // console.log( car.name + ' by ' + car.manufacturer)
+        console.log(this)
     }
     
 }
