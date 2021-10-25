08949feae4a8baf7419984dd1b4c83c574b91438
Sahil Saini <thisissahilsaini@gmail.com>
2021-10-25 23:09:13 +0400

    Fetch API Intro

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
+//'fetch' fetches the data from web 
 
-//This function perform ta sk after a particular time
-setTimeout( () =>{
-    console.log("Sahil")
-}, 3000)
+var data = fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-console.log('Hi')
+console.log(data)
