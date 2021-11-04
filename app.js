af429fe75aff18a91f26393c63e48434507ea3a2
Sahil Saini <thisissahilsaini@gmail.com>
2021-11-04 11:50:15 +0400

    .then method

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-let value = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Hello')
-
-    }, 1000)
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(data => {
+    console.log(data)
 })
 
-console.log(value)
 
-setTimeout( () => {
-    console.log(value)
-}, 2000)
-
-
-
-//Here the new keyword will create new promise
-//Promise is a object
-//With promise we have to pass two parameters resolve & reject
-//Resolve: Once the promise is complete
-//Reject: If the promise is incomplete
-
-/* Promise has 3 states
-
-1. Fulfil
-2. Pending 
-3. Rejected
-
-*/
+//.then method: 
