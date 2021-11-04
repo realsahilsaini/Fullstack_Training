97d52d7bd192086aa4e7b2a3df06f345cd56fae1
Sahil Saini <thisissahilsaini@gmail.com>
2021-11-04 11:12:00 +0400

    Promises

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-//'fetch' fetches the data from web 
+let value = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Hello')
 
-var data = fetch('https://jsonplaceholder.typicode.com/todos/1')
+    }, 1000)
+})
 
-console.log(data)
+console.log(value)
+
+setTimeout( () => {
+    console.log(value)
+}, 2000)
+
+
+
+//Here the new keyword will create new promise
+//Promise is a object
+//With promise we have to pass two parameters resolve & reject
+//Resolve: Once the promise is complete
+//Reject: If the promise is incomplete
+
+/* Promise has 3 states
+
+1. Fulfil
+2. Pending 
+3. Rejected
+
+*/
