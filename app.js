ecc9a4d8b37cc64b111e1b30f14b848d785f5243
Sahil Saini <thisissahilsaini@gmail.com>
2021-11-06 13:53:50 +0400

    async ( )

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-//Selecting table
-const table = document.querySelector('table')
-console.log(table)
+// This will give error because, here we are not waiting for the Promise to resolve
+// const response = fetch('https://jsonplaceholder.typicode.com/users')
 
+// const data = response.json()
 
+// console.log(data)
 
+// Correct method 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
-// .tden(data => {console.log(data)} )
-.then(data => {
+const fetchdata = async () => {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    console.log(response)
+    
+    const data = await response.json()
+
     console.log(data)
-    data.forEach(user => {
-        let newRow = document.createElement('tr')
 
-        // let dataID = document.createElement('td')
-        // let dataID_textNode = document.createTextNode(user.id)
-        // dataID.appendChild(dataID_textNode)
-        // newRow.appendChild(dataID)
-        // table.appendChild(newRow)
-       
-        // Above code is just for user's id. That mean we have to write same code for name,username, email
+}
 
-
-
-        // OR 
-
-        //Following is the efficient code
-
-        // Here we are making a template  
-        let  element = `
-        <td>${user.id}</td>
-        <td>${user.name}</td>
-        <td>${user.username}</td>
-        <td>${user.email}</td>`
-        console.log(element)
-        
-        newRow.innerHTML = element
-        table.appendChild(newRow)
-        
-    })
-})
-
+fetchdata()
