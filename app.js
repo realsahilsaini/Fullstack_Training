0cd0dd04f6aff4e88db7ecddfa35295f20a24dd0
Sahil Saini <thisissahilsaini@gmail.com>
2021-12-03 14:56:41 +0400

    Localstorage: Using stringify and parse

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-// console.log(localStorage)
+// How to store array in local storage 
 
-// localStorage.setItem('InputValue', "Something")
-// console.log(localStorage.getItem("InputValue")) 
-
-// To clear local storage we have two methods:
-// 1. Either to do it manually via applications 
-// 2. localStorage.clear()
-
-// localStorage.clear()
-
-// ----------------------------------------------
 
 const button = document.querySelector('button')
 const input = document.querySelector('input')
 const div = document.querySelector('div')
 
-div.innerHTML = localStorage.getItem('InputValue')
+// localStorage.clear()
 
-button.addEventListener('click', (req, res) => {
-    localStorage.setItem('InputValue', input.value)
-    div.innerHTML = input.value
-})
+const arr = [1, 2, 3, 4, 5]
+
+// localStorage.setItem('val', arr)
+
+//Local storage apparently only supports string
+//But yet the arr will be displayed as string
+// console.log(localStorage.getItem('val'))
+
+// so there is a in-built function that does this job input i.e to give output in the array
+// localStorage.setItem('val', JSON.stringify(arr))
+// It is still a string in the console log 
+// console.log(localStorage.getItem('val'))
+
+// To get back to its form 
+// console.log(JSON.parse(localStorage.getItem('val')))
