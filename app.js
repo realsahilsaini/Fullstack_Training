279a09dfbb346a4d5c011fd3d9b78815d756f3c6
Sahil Saini <thisissahilsaini@gmail.com>
2021-12-21 13:06:03 +0400

    Modules: path

--- app.js
+++ app.js
@@ -6,9 +6,30 @@ const path = require('path')
 
 // console.log(path)
 
+//------------------------------------
+
 // console.log(path.resolve('./utils.js'))
 //resolve converts an relative part to absolute path 
 
-console.log(path.relative('../', "./utils.js"))
+//------------------------------------
+
+// console.log(path.relative('./', "./utils.js"))
 //1st parameter: With what is should be relative (here it is last to last folder './')
+// "/" denotes the root
 // 2nd parameter: file location
+
+//------------------------------------
+
+// console.log(path.extname('./utils.js'))
+
+//------------------------------------
+
+// console.log(path.dirname('./utils.js'))
+
+//------------------------------------
+
+// console.log(__dirname) 
+
+//------------------------------------
+
+// console.log(path.join(__dirname, 'app.js'))
